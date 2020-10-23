6119c04cf3b6a034432a53db4772b870122c33f4
Lanrna Sayasean <lsayasean@gmail.com>
2020-10-23 10:02:25 -0400

    unit testing practice

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
-var chai = require('chai')
-var should = require('chai').should()
+var chai = require("chai");
+var should = require("chai").should();
+var assert = chai.assert;
 var expect = chai.expect;
 var Add = require("../math");
 
@@ -11,16 +12,31 @@ describe("addition test", function () {
     var expectecResults = 3;
     var actualResult = Add(numOne, numTwo);
 
-    actualResult.should.equal(expectecResults)
-    expect(actualResult).to.equal(expectecResults)
+    actualResult.should.equal(expectecResults);
+    expect(actualResult).to.equal(expectecResults);
   });
-  it("should not return 3 when passed one and four", function(){
-      let numOne = 1;
-      let numTwo = 4;
+  it("should not return 3 when passed one and four", function () {
+    let numOne = 1;
+    let numTwo = 4;
 
-      let notExpected = 3
-      let actualResult = Add(numOne, numTwo)
+    let notExpected = 3;
+    let actualResult = Add(numOne, numTwo);
 
-      expect(actualResult).to.not.equal(notExpected)
+    expect(actualResult).to.not.equal(notExpected);
+  });
+  it("should equal add function", () => {
+    let numberOne = 5;
+    let numberTwo = 10;
+
+    let respectedTotal = 15;
+    let actualResults = Add(numberOne, numberTwo)
+    expect(respectedTotal).to.equal(actualResults)
+  });
+  it("assert to not equal", () => {
+    let numberOne = 5;
+    let numberTwo = 10;
+    let respectedTotal = 15;
+    let actualResults = Add(numberOne, numberTwo)
+    assert.equal(respectedTotal, actualResults)
   })
 });
